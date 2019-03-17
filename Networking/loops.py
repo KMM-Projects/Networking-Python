9dfb2325aafbb6f0b426c2a73658c260605f0a0a
Patrik Kemeny <patrikkemeny@192-168-1-2.tpgi.com.au>
2019-03-17 13:35:04 +1100

    loops.py

---
+++ Networking/loops.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+__author__ = 'Patrik'
+
+name=input("What is your name?")
+
+# for loop ...
+for i in range(1,10):
+    print(name,i)
+
+# while loop ...
+x=0
+while True:
+    print(x)
+    x=x+1
+    if (x==15):
+        break
+
+# for loop using a list
+mylist=['lions','tigers','bears','eagle']
+for i in mylist:
+    print(i)
+
+
+
